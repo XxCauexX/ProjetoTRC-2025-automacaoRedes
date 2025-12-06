ea2807b275a502b040d6664edf6b957c2f049e40
Caue <caue.andrade.505@gmail.com>
2025-12-06 19:26:51 -0300

    Adicionando comentario com integrantes do grupo

--- js/pagina2.js
+++ js/pagina2.js
@@ -1,12 +1,16 @@
+/* 
+    Integrantes: Cauê de Andrade, Matheus Amaral Abdala, Eduardo Oliveira Ventura, Isabella Carregoza Silva.
+*/
+
 const buttons = document.querySelectorAll('.toggle-btn');
 
-        buttons.forEach(btn => {
-            btn.addEventListener('click', () => {
-                const card = btn.parentElement;
-                const moreText = card.querySelector('.read-more');
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const card = btn.parentElement;
+        const moreText = card.querySelector('.read-more');
 
-                const isVisible = moreText.style.display === "block";
-                moreText.style.display = isVisible ? "none" : "block";
-                btn.textContent = isVisible ? "Saiba mais" : "Ler menos";
-            });
-        });
+        const isVisible = moreText.style.display === "block";
+        moreText.style.display = isVisible ? "none" : "block";
+        btn.textContent = isVisible ? "Saiba mais" : "Ler menos";
+    });
+});
